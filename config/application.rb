ab49398e0b3a4ff0e84ceb04c495bea16ef8821b
Avidor Turkewitz <aviturkewitz@gmail.com>
2015-11-25 09:21:03 -0500

    Modify application.rb to match steves

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,13 @@ Bundler.require(*Rails.groups)
 
 module JsOrBsServer2
   class Application < Rails::Application
-    config.middleware.use Rack::Cors do
-      allow do
-        origins "*"
-        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
-      end
-    end
+
+    // Taken from stevens boozer api
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
