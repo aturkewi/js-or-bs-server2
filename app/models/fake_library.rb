270335a78ddd2f4708ff71cacd35dc240514b586
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-15 18:48:47 -0400

    Add extra logic to fake lib name

--- app/models/fake_library.rb
+++ app/models/fake_library.rb
@@ -2,7 +2,11 @@ class FakeLibrary < ActiveRecord::Base
   after_initialize :add_name
 
   def add_name
-    self.name=Faker::Hipster.words(1,false,true)[0]
+    self.name=Faker::Hipster.words(1,false,true)[0].split(" ").map(&:capitalize).join(" ") + extra_js
+  end
+
+  def extra_js
+    rand() > .75 ? ".js" : ""
   end
 
 end
